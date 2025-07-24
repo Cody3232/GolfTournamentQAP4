3c1ecb3c720006cb3e0044c90050360dcce94e67
Cody Snow <codysnow3@gmail.com>
2025-07-24 21:29:47 -0230

    Fixed consructor, within member.java (used new, but i only need that for creating objects)

---
+++ src/main/java/golftournament/Member.java
@@ -0,0 +1,65 @@
+package golftournament;
+
+import javax.persistence.*;
+import java.util.Date;
+
+//Show annotations
+@Entity
+@Table
+public class Member {
+    //Declare fields
+    @Id // Mark primary key field
+    @GeneratedValue // Auto generate ID
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String address;
+
+    @Column
+    private String email;
+
+    //Constructors (Default and with fields)
+    public Member() {}
+
+    public Member(int id, String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    // Getters/Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/golftournament/Tournament.java
+++ src/main/java/golftournament/Tournament.java
@@ -1,4 +1,4 @@
-package com;
+package golftournament;
 
 public class Tournament {
 }
